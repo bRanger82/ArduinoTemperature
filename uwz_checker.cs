daf577eec287a1f100cc7b6229a7d81c23bb2191
Michael <bRanger82@users.noreply.github.com>
2017-06-20 12:30:27 +0200

    Checks the status of uwz.at
    Gets the map with the actual warnings. As these warnings are shown in different colors (green - no warning, yellow - prewarning, ....) the map is downloaded as image. Then the getPixel is used to get the color for a specific region (use Paint.net before to get the Pixel-Location of the place you life).

---
+++ uwz_checker.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wgetImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer(performCheck, null, 0, 15000);
+            
+            Console.ReadKey();
+        }
+
+        private static void performCheck(Object o)
+        {
+            string filename = @"C:\temp\warn.png";
+            string url = "http://www.uwz.at/at/de/incoming/maps_warnings/at/AT_all_warnings_484x242.png";
+
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                }
+
+                bool bOK = DownloadRemoteImageFile(url, filename);
+
+                if (!bOK)
+                {
+                    Console.WriteLine("Error while downloading image from the internet!");
+                    return;
+                }
+
+                // Create a Bitmap object from an image file.
+                Bitmap myBitmap = (Bitmap)Image.FromFile(filename);
+                // Get the color of a pixel within myBitmap.
+                Color pixelColor = myBitmap.GetPixel(227, 126); //Hallein
+                //Color pixelColor = myBitmap.GetPixel(215, 168); //Beispiel fuer Gelb (Gebiet hatte zu diesem Zeitpunkt einen Warnung - Gelb)
+                if (pixelColor.R == 0 && pixelColor.G == 255 && pixelColor.B == 0)
+                {
+                    Console.WriteLine("Weather OK - " + pixelColor.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("!!! *** WEATHER WARNING *** !!! - " + pixelColor.ToString());
+                }
+
+                myBitmap.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            } finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private static bool DownloadRemoteImageFile(string uri, string fileName)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Check that the remote file was found. The ContentType
+            // check is performed since a request for a non-existent
+            // image file might be redirected to a 404-page, which would
+            // yield the StatusCode "OK", even though the image was not
+            // found.
+            if ((response.StatusCode == HttpStatusCode.OK ||
+                response.StatusCode == HttpStatusCode.Moved ||
+                response.StatusCode == HttpStatusCode.Redirect) &&
+                response.ContentType.StartsWith("image", StringComparison.OrdinalIgnoreCase))
+            {
+
+                // if the remote file was found, download oit
+                using (Stream inputStream = response.GetResponseStream())
+                using (Stream outputStream = File.OpenWrite(fileName))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    do
+                    {
+                        bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                        outputStream.Write(buffer, 0, bytesRead);
+                    } while (bytesRead != 0);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
